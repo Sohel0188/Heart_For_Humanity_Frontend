21175fe8612b8be45592ad584e9a51778d5185a9
Sohel0188 <sohelsikder.cse@gmail.com>
2025-05-24 01:44:30 +0600

    event responsibe correction

--- custom_js/app.js
+++ custom_js/app.js
@@ -1,57 +1,3 @@
-// fetch("https://heart-for-humanity.vercel.app/campain/all_capain/")
-// .then(res=>res.json())
-// .then(data=>{
-//     // console.log(data);
-    
-//     data.forEach(element => {
-//         let raised_money_in_parcent = (element.raised_price*100)/element.goal_price;
-//         let infloor = Math.floor(raised_money_in_parcent)
-//         // console.log(raised_money_in_parcent);
-//         // console.log(infloor);
-//         const parents = document.getElementById('cause-carousel');
-//         const div = document.createElement("div")
-//         div.classList.add('cause-block-one');
-//         div.innerHTML=`
-                        
-//                         <div class="inner-box">
-//                             <div class="image"><a href="cause-details.html?slug=${element.campain_slug}"><img src="${element.image}" alt=""></a></div>
-//                             <div class="lower-content">
-//                                 <h4><a href="cause-details.html?slug=${element.campain_slug}">${element.campain_title}</a></h4>
-//                                 <div class="category"><a href="#">${element.category_name}</a></div>
-//                                 <div class="text short-details">${element.short_details}</div>
-//                                 <div class="info-box">
-//                                     <a href="#"><span>Raised:</span>৳ ${element.raised_price}</a>
-//                                     <a href="#"><span>Goal:</span>৳ ${element.goal_price}</a>
-//                                 </div>
-//                                 <!--Progress Levels-->
-//                                 <div class="progress-levels">
-                                            
-//                                     <!--Skill Box-->
-//                                     <div class="progress-box wow fadeInRight" data-wow-delay="100ms" data-wow-duration="1500ms">
-//                                         <div class="inner">
-//                                             <div class="bar">
-//                                                 <div class="bar-innner"><div class="bar-fill" data-percent="${infloor}"><div class="percent"></div></div></div>
-//                                             </div>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div class="bottom-content">
-//                                     <div class="link-btn"><a href="cause-details.html?${element.campain_slug}" class="theme-btn btn-style-one donate-box-btn" onclick="getID(${element.id})"><span>Donate Now</span></a></div>
-//                                     <div class="share-icon post-share-icon d-none">
-//                                         <div class="share-btn"><i class="flaticon-share"></i></div>
-//                                         <ul>
-//                                             <li><a href="#"><span class="fa fa-facebook"></span></a></li>
-//                                             <li><a href="#"><span class="fa fa-google-plus"></span></a></li>
-//                                         </ul>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </div>
-                    
-//         `
-//         parents.appendChild(div);
-//     });
-// })
 document.addEventListener("DOMContentLoaded", function () {
     fetch("https://heart-for-humanity.vercel.app/campain/all_capain/")
         .then(res => res.json())
@@ -123,24 +69,24 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 fetch("https://heart-for-humanity.vercel.app/events/all_events/")
-.then(res=>res.json())
-.then(data=>{
-    // console.log(data);
-    
-    data.forEach(element => {
-    const inputDate = element.event_data;
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data);
 
-        const dateParts = inputDate.split("-");
-        const year = parseInt(dateParts[2], 10);
-        const month = parseInt(dateParts[1], 10) - 1;
-        const day = parseInt(dateParts[0], 10);
-        const dateObject = new Date(year, month, day);
-        const dayNumber = dateObject.getDate();
-        const monthName = dateObject.toLocaleString('default', { month: 'long' });
-        const parents = document.getElementById('event-carousel');
-        const div = document.createElement("div")
-        div.classList.add('event-block-one');
-        div.innerHTML=`
+        data.forEach(element => {
+            const inputDate = element.event_data;
+
+            const dateParts = inputDate.split("-");
+            const year = parseInt(dateParts[2], 10);
+            const month = parseInt(dateParts[1], 10) - 1;
+            const day = parseInt(dateParts[0], 10);
+            const dateObject = new Date(year, month, day);
+            const dayNumber = dateObject.getDate();
+            const monthName = dateObject.toLocaleString('default', { month: 'long' });
+            const parents = document.getElementById('event-carousel');
+            const div = document.createElement("div")
+            div.classList.add('event-block-one');
+            div.innerHTML = `
                 <div class="inner-box">
                                         <div class="image"><img src="${element.event_image}" alt=""></div>
                                         <div class="lower-content">
@@ -155,29 +101,44 @@ fetch("https://heart-for-humanity.vercel.app/events/all_events/")
                                     </div>        
                     
         `
-        parents.appendChild(div);
-    });
-})
+            parents.appendChild(div);
+        });
+       
+        
+        $("#event-carousel").owlCarousel({
+            loop: true,
+            margin: 30,
+            nav: false,
+            dots: true,
+            items: 3,
+            responsive: {
+                0: { items: 1 },
+                768: { items: 2 },
+                992: { items: 3 }
+            }
+        });
+
+    })
 
 
 fetch("https://heart-for-humanity.vercel.app/blog/all_blog/")
-.then(res=>res.json())
-.then(data=>{
-    // console.log(data);
-    
-    data.forEach(element => {
-        const inputDate = element.created_date
-        const dateObject = new Date(inputDate);
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data);
 
-        const formattedDate = dateObject.toLocaleString('en-US', {
-        month: 'short', 
-        day: 'numeric', 
-        year: 'numeric', 
-        });
-        const parents = document.getElementById('blog-grid');
-        const div = document.createElement("div")
-        div.classList.add('col-lg-3' ,'col-md-6', 'news-block-one');
-        div.innerHTML=`                        
+        data.forEach(element => {
+            const inputDate = element.created_date
+            const dateObject = new Date(inputDate);
+
+            const formattedDate = dateObject.toLocaleString('en-US', {
+                month: 'short',
+                day: 'numeric',
+                year: 'numeric',
+            });
+            const parents = document.getElementById('blog-grid');
+            const div = document.createElement("div")
+            div.classList.add('col-lg-3', 'col-md-6', 'news-block-one');
+            div.innerHTML = `                        
                         <div class="inner-box wow fadeInUp" data-wow-delay="200ms">
                         <div class="category"><a href="#">${element.category_name}</a></div>
                         <div class="image">
@@ -197,15 +158,15 @@ fetch("https://heart-for-humanity.vercel.app/blog/all_blog/")
                     </div>
                     
         `
-        parents.appendChild(div);
-    });
-})
+            parents.appendChild(div);
+        });
+    })
 
-function getID(id){
+function getID(id) {
     document.getElementById("campain_id").value = id
 }
 
-const donet = (event) => {    
+const donet = (event) => {
     event.preventDefault();
     const campaign = localStorage.getItem("campaign_id");
     // const campaign = new URLSearchParams(window.location.search).get("Id");
@@ -216,7 +177,7 @@ const donet = (event) => {
     const amount = document.getElementById("other-amount").value;
     // const campaign = document.getElementById("campain_id").value;
     const user = localStorage.getItem("user_id");
-    
+
     console.log(campaign);
     const userValue = user ? user : 0;
     const info = {
@@ -230,55 +191,55 @@ const donet = (event) => {
 
     console.log(info);
 
-    fetch("https://heart-for-humanity.vercel.app/campain/donetion/",{
+    fetch("https://heart-for-humanity.vercel.app/campain/donetion/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(info),
     })
-    .then((res) => res.json())
-    .then((data) => {        
-        console.log(data);
-        const donation_id = data.id;
-        console.log(donation_id);
-        // Show success message
-        //const successMessage = document.getElementById("success-message");
-        //successMessage.innerText = "Donation Successful! Thank you!";
-        console.log("Success");
-        //successMessage.classList.add("show-message"); // Add class for visibility
-        // Add class to donate popup
-        // document.querySelector(".donate-popup").classList.add("active");
-        // Clear input fields
-        document.getElementById("donar_name").value = "";
-        document.getElementById("donar_email").value = "";
-        document.getElementById("donar_phone").value = "";
-        document.getElementById("other-amount").value = "";
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            const donation_id = data.id;
+            console.log(donation_id);
+            // Show success message
+            //const successMessage = document.getElementById("success-message");
+            //successMessage.innerText = "Donation Successful! Thank you!";
+            console.log("Success");
+            //successMessage.classList.add("show-message"); // Add class for visibility
+            // Add class to donate popup
+            // document.querySelector(".donate-popup").classList.add("active");
+            // Clear input fields
+            document.getElementById("donar_name").value = "";
+            document.getElementById("donar_email").value = "";
+            document.getElementById("donar_phone").value = "";
+            document.getElementById("other-amount").value = "";
 
-        setTimeout(() => {
-       
-           window.location.href = `https://heart-for-humanity.vercel.app/campain/payment/?donation_id=${donation_id}`;
-        }, 2000);
+            setTimeout(() => {
+
+                window.location.href = `https://heart-for-humanity.vercel.app/campain/payment/?donation_id=${donation_id}`;
+            }, 2000);
 
 
-    })
-    .catch((error) => {
-        console.error("Error:", error);
-        alert("Something went wrong. Please try again.");
-    });
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            alert("Something went wrong. Please try again.");
+        });
 };
 
 fetch("https://heart-for-humanity.vercel.app/account/list/")
-.then(res => res.json())
-.then(data => {
-    const parents = document.getElementById('donar-list');
-    parents.innerHTML = "";
+    .then(res => res.json())
+    .then(data => {
+        const parents = document.getElementById('donar-list');
+        parents.innerHTML = "";
 
-    data.forEach(element => {
-        parents.innerHTML += `
+        data.forEach(element => {
+            parents.innerHTML += `
             <div class="col-md-4 donor-block">
                 <div class="inner-box wow fadeInUp" data-wow-delay="200ms">
                     <div class="top-content">
                         <div class="image">
-                            <img class ="donar-image" src="${element.profile_image}" alt="">
+                            <img class ="donar-image" style="width:100px" src="${element.profile_image}" alt="">
                             <div class="overlay">
                                 <div class="icon"><a href="#"><span class="fa fa-twitter"></span></a></div>
                             </div>
@@ -293,6 +254,6 @@ fetch("https://heart-for-humanity.vercel.app/account/list/")
                 </div>
             </div>
         `;
-    });
-})
-.catch(error => console.error("Error fetching data:", error));
+        });
+    })
+    .catch(error => console.error("Error fetching data:", error));
